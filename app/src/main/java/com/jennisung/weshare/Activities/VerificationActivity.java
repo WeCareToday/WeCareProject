9a379d518b580ebadba68c02274c3f54b3abda5f
Jennifer Sung <sung.jenni93@gmail.com>
2023-09-11 17:36:19 -0400

    add activities for signup,login, profile

---
+++ app/src/main/java/com/jennisung/weshare/Activities/VerificationActivity.java
@@ -0,0 +1,16 @@
+package com.jennisung.weshare.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jennisung.weshare.R;
+
+public class VerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+    }
+}
