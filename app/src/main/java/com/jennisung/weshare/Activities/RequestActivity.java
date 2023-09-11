c41460805a6ab778ce885c3286a9fe45168e879c
Jennifer Sung <sung.jenni93@gmail.com>
2023-09-11 17:43:41 -0400

    add donate food and request acitvities

---
+++ app/src/main/java/com/jennisung/weshare/Activities/RequestActivity.java
@@ -0,0 +1,16 @@
+package com.jennisung.weshare.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jennisung.weshare.R;
+
+public class RequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request);
+    }
+}
