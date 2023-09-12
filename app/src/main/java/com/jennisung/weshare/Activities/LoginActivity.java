f0048d500286f429a1e35017aae4f31bc5857995
Kyle Knight <knightkyle@outlook.com>
2023-09-11 21:54:33 -0700

    Update login without touching AWS

--- app/src/main/java/com/jennisung/weshare/Activities/LoginActivity.java
+++ app/src/main/java/com/jennisung/weshare/Activities/LoginActivity.java
@@ -1,16 +1,41 @@
 package com.jennisung.weshare.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 import com.jennisung.weshare.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if(username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Please enter both username and password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // AWS backend
+
+            }
+        });
     }
 }
