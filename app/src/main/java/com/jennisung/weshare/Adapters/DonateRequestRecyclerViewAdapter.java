77c79d0cc97a8b5eb4fe92c40f0f8d19fa210ef1
Jennifer Sung <sung.jenni93@gmail.com>
2023-09-13 17:17:51 -0400

    adding recycler view

---
+++ app/src/main/java/com/jennisung/weshare/Adapters/DonateRequestRecyclerViewAdapter.java
@@ -0,0 +1,29 @@
+package com.jennisung.weshare.Adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+//TODO:step 1-4: Make a custom recyclerview adapter that extends
+public class DonateRequestRecyclerViewAdapter extends RecyclerView.Adapter {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        TODO: step 1-7: inflating the fragment(add the fragment to the viewholder)
+//        View donateRequestFragmenets = layout
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
