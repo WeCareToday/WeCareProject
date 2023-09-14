43f3711d734d909c828c42953dbab96a7e0f303a
Juan C Olmedo <doubleparked88@gmail.com>
2023-09-13 17:32:04 -0700

    donate food activity works now able to upload to aws

--- app/src/main/java/com/jennisung/weshare/Activities/DonateFoodActivity.java
+++ app/src/main/java/com/jennisung/weshare/Activities/DonateFoodActivity.java
@@ -1,16 +1,25 @@
 package com.jennisung.weshare.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.FoodListing;
 import com.jennisung.weshare.R;
 
 public class DonateFoodActivity extends AppCompatActivity {
+
+    public static String TAG = "Donate-Activity";
+    private String currentUserID = "";
 
     private EditText availableFoodItemsEditText;
     private CheckBox isWillingToDeliverCheckBox;
@@ -38,6 +47,7 @@ public class DonateFoodActivity extends AppCompatActivity {
     }
 
     private void submitFoodListing() {
+        setCurrentUserID();
         String availableFoodItems = availableFoodItemsEditText.getText().toString().trim();
         boolean isWillingToDeliver = isWillingToDeliverCheckBox.isChecked();
         boolean isAvailableForPickup = isAvailableForPickupCheckBox.isChecked();
@@ -51,7 +61,38 @@ public class DonateFoodActivity extends AppCompatActivity {
         // Here, create a FoodListing object and use AWS Amplify or another method to store this data in your backend
         // FoodListing newListing = new FoodListing(...);
         // ...
+        FoodListing foodListingToSave = FoodListing.builder()
+                .availableFoodItems(availableFoodItems)
+                .userId(currentUserID)
+                .isWillingToDeliver(isWillingToDeliver)
+                .isAvailableForPickup(isAvailableForPickup)
+                .contactEmail(contactEmail)
+                .build();
 
+        saveToDynamo(foodListingToSave);
         Toast.makeText(this, "Food listing submitted!", Toast.LENGTH_SHORT).show();
+        Intent backToProfileActivity = new Intent(DonateFoodActivity.this, ProfileActivity.class);
+        startActivity(backToProfileActivity);
+    }
+
+    private void saveToDynamo(FoodListing foodListingToSave){
+        Amplify.API.mutate(
+                ModelMutation.create(foodListingToSave),
+                success -> Log.i(TAG, "able to upload to database"),
+                failure -> Log.i(TAG, "Could upload to database error: " + failure)
+
+        );
+    }
+
+    void setCurrentUserID(){
+        Amplify.Auth.getCurrentUser(
+                success -> {
+                    currentUserID = success.getUserId().toString();
+                    Log.i(TAG, "Authenticated user is "+ success.getUserId().toString());
+                },
+                failure -> {
+                    Log.i(TAG, "Could not find authenticated");
+                }
+        );
     }
 }
